bf9575f45ae95c1c911e805097e1b42fb73c64ee
Tsurkan Georgiy <egorizz@mail.ru>
2023-06-30 17:08:48 +0500

    Убрал отступы у body

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(window).scroll(function() {
+//    if ($(window).scrollTop() >= 300) {
+//        $('header').addClass('fixed');
+//    }
+//    else {
+//        $('header').removeClass('fixed');
+//    }
+// });
+
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ {
+//         $('.section-outer').addClass('scrolled');
+//     } else {
+//         $('.section-outer').removeClass('scrolled');
+//     }
+// });
