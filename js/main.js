7a99fb8259e494ad75744093c1cd4e7194407c46
Tsurkan Georgiy <egorizz@mail.ru>
2023-07-03 19:29:05 +0500

    Сделал доп страницы

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@
 //         $('.section-outer').removeClass('scrolled');
 //     }
 // });
+function changeTab(tabIndex) {
+    // Получаем все вкладки и их содержимое
+    var tabs = document.getElementsByClassName('tab');
+    var contentTabs = document.getElementsByClassName('content-tab');
+  
+    // Удаляем класс "active" у всех вкладок и их содержимого
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove('active');
+      contentTabs[i].classList.remove('active');
+    }
+  
+    // Добавляем класс "active" выбранной вкладке и ее содержимому
+    tabs[tabIndex - 1].classList.add('active');
+    contentTabs[tabIndex - 1].classList.add('active');
+  }
