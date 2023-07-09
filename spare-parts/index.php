44dcfe763e961f28bf5f4e7bb050ac7cfcf1150b
egorizz <egorizz@mail.ru>
2023-07-09 18:48:09 +0500

    Сделал импуты

---
+++ spare-parts/index.php
@@ -0,0 +1,51 @@
+<? include __DIR__.'\..\header.php' ?>
+
+    <!-- MAIN -->
+    <main>
+        <!-- EQUIPMENT -->
+        <div class="section-outer outer-equipmen">
+            <div class="section-inner">
+                <div class="bread-crumbs">
+                    <div class="bread-crumbs-title">Главная</div>
+                    <div class="bread-crumbs-title">Запасные части</div>
+                </div>
+            </div>
+
+            <div class="options">
+                <div class="options-wrap">
+                    <div class="material">
+                        <h1>Запасныые части</h1>
+                        <div class="material-text">
+                            Пуансоны используются в процессе прессования для нанесения определенного рисунка на
+                            материал, например, таблетку. Это необходимая деталь таблетпресса.
+                        </div>
+                    </div>
+
+                    <div class="grid-container">
+                        <div class="grid-item">
+                            <div class="img-wraper">
+                                <a href="/spare-parts/spare-parts.html" class="grid-item-img grid-item-img_alt"><img src="/img/p1.jpg"
+                                        alt=""></a>
+
+                            </div>
+                            <a href="/spare-parts/spare-parts.html" class="grid-item-text">Запасные части</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <a href="#" class="container_item1">
+                        <img src="../img/bannerbad2.jpeg" alt="" class="container_item1-img1">
+                    </a>
+                    <a href="#" class="container_item2">
+                        <img src="../img/banner-line2.jpg" alt="" class="container_item2-img2">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- / EQUIPMENT -->
+    </main>
+    <!-- / MAIN -->
+
+
+    <? include __DIR__.'\..\footer.php' ?>
